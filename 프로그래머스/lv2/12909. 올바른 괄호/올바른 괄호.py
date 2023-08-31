bc476fd0c566f0d16e2d4d7c1479e61be2edd5d6
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-01 01:06:20 +0900

    [level 2] Title: 올바른 괄호, Time: 6.94 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,10 @@
 def solution(s):
     cnt=0
-    if s[0]==')':
-        return False
     for i in s:
         if i=='(':
             cnt+=1
-        else:
+        elif i==')':
             cnt-=1
-            if cnt<0:
-                return False
-    if cnt==0:
-        return True
-    else:
-        return False
+        if cnt<0:
+            return False
+    return cnt==0
