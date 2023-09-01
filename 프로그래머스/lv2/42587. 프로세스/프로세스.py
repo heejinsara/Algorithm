183da17e055156cbc47bfdcf28f71f177f08d9c8
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-01 14:21:01 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -0,0 +1,11 @@
+def solution(priorities, location):
+    que=[(i,j) for i,j in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur=que.pop(0)
+        if any(cur[1]<q[1] for q in que):
+            que.append(cur)
+        else:
+            answer+=1
+            if cur[0]==location:
+                return answer
