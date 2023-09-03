b91998a0cfc0ad3937d74d530a19a116cad65d6a
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-03 20:52:08 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+def solution(k, dungeons):
+    answer=[]
+    for i in permutations(dungeons,len(dungeons)):
+        temp=k
+        cnt=0
+        for j in i:
+            if temp>=j[0]:
+                temp-=j[1]
+                cnt+=1
+        answer.append(cnt)
+    return max(answer)
