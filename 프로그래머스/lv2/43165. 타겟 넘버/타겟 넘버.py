17577f09a14f349a1e8d01d28b00c89ace19224e
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-04 18:51:47 +0900

    [level 2] Title: 타겟 넘버, Time: 4.25 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,4 @@
+'''
 def solution(numbers, target):
     leaves=[0]
     for num in numbers:
@@ -8,3 +9,19 @@ def solution(numbers, target):
         leaves=temp
     answer=leaves.count(target)
     return answer
+'''
+cnt=0
+def DFS(numbers, target, idx, value):
+    global cnt
+    if idx==len(numbers) and value==target:
+        cnt+=1
+        return
+    elif idx==len(numbers):
+        return
+    DFS(numbers, target, idx+1, value+numbers[idx])
+    DFS(numbers, target, idx+1, value-numbers[idx])
+
+def solution(numbers, target):
+    global cnt
+    DFS(numbers, target, 0, 0)
+    return cnt
