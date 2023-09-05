3492cbc95211286954ace668cb1874e14feac77f
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-05 17:22:38 +0900

    [level 2] Title: 타겟 넘버, Time: 4.99 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,27 +1,15 @@
-'''
-def solution(numbers, target):
-    leaves=[0]
-    for num in numbers:
-        temp=[]
-        for leaf in leaves:
-            temp.append(leaf-num)
-            temp.append(leaf+num)
-        leaves=temp
-    answer=leaves.count(target)
-    return answer
-'''
-cnt=0
-def DFS(numbers, target, idx, value):
-    global cnt
+answer=0
+def dfs(numbers, target, idx, value):
+    global answer
     if idx==len(numbers) and value==target:
-        cnt+=1
+        answer+=1
         return
     elif idx==len(numbers):
         return
-    DFS(numbers, target, idx+1, value+numbers[idx])
-    DFS(numbers, target, idx+1, value-numbers[idx])
+    dfs(numbers, target, idx+1, value-numbers[idx])
+    dfs(numbers, target, idx+1, value+numbers[idx])
 
 def solution(numbers, target):
-    global cnt
-    DFS(numbers, target, 0, 0)
-    return cnt
+    global answer
+    dfs(numbers, target, 0, 0)
+    return answer
