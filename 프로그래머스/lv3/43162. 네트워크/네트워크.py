50da8c2205a6ab31d6c5c013353f47d4d133b950
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-05 19:51:14 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,15 +1,16 @@
+def dfs(n, computers, com1, visited):
+    visited[com1]=1
+    for com2 in range(n):
+        if com1!=com2 and computers[com1][com2]==1 and visited[com2]==0:
+            dfs(n, computers, com2, visited)
+            
+    
+
 def solution(n, computers):
-    answer = 0
     visited=[0]*n
-    for com in range(n):
-        if visited[com]==0:
-            DFS(n, computers, com, visited)
+    answer=0
+    for com1 in range(n):
+        if visited[com1]==0:
+            dfs(n, computers, com1, visited)
             answer+=1
     return answer
-
-def DFS(n, computers, com, visited):
-    visited[com]=1
-    for connect in range(n):
-        if com!=connect and computers[com][connect]==1:
-            if visited[connect]==0:
-                DFS(n, computers, connect, visited)
