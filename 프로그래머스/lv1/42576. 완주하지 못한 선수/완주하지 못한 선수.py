5751f041464f5dd5fa1cffb4c837e512fe5c2ab4
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-24 03:17:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.08 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,7 @@
-def solution(participant, completion):
+def solution(participant,completion):
     participant.sort()
     completion.sort()
     for i,j in zip(participant,completion):
         if i!=j:
-            print(i)
             return i
     return participant[-1]
