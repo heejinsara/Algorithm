aa741664b45c2fa7389cc234266a7d1a768069f1
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-24 03:33:03 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,10 +1,9 @@
 from collections import defaultdict
-from itertools import combinations, permutations
 def solution(clothes):
     answer=1
     D=defaultdict(list)
-    for i in clothes:
-        D[i[1]].append(i[0])
+    for c in clothes:
+        D[c[1]].append(c[0])
     for key in D.keys():
         answer*=(len(D[key])+1)
     return answer-1
