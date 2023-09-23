6458f07b13ed084c138a5c1938bf1a0af975a536
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-09-24 00:28:20 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,18 +1,18 @@
 import math
-def solution(progresses, speeds):
+def solution(progresses,speeds):
     result=[]
     answer=[]
-    for i,j in zip(progresses, speeds):
-        result.append(math.ceil((100-i)/j))
-    num=result[0]
-    cnt=0
-    for k in result:
-        if k<=num:
+    for i in range(len(progresses)):
+        result.append(math.ceil((100-progresses[i])/speeds[i]))
+    print(result)
+    cnt=1
+    temp=result[0]
+    for j in result[1:]:
+        if temp>=j:
             cnt+=1
         else:
             answer.append(cnt)
-            num=k
-            cnt=0
-            cnt+=1
+            temp=j
+            cnt=1
     answer.append(cnt)
     return answer
