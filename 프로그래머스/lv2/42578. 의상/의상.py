6f5875dfc7c00cc3de4347adf89c2e51aeaa4988
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-07 00:27:50 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
-def solution(clothes):
-    answer=1
+def solution(종류):
     D=defaultdict(list)
-    for c in clothes:
-        D[c[1]].append(c[0])
-    for key in D.keys():
-        answer*=(len(D[key])+1)
+    for i in 종류:
+        D[i[1]].append(i[0])
+    answer=1
+    for v in D.values():
+        answer*=(len(v)+1)
     return answer-1
