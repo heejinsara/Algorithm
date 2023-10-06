6382048a2c0669bc86bb6dbed89a0f8e28e446e1
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-07 02:43:46 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,6 +1,14 @@
+# import numpy as np
+# def solution(citations):
+#     citations.sort(reverse=True)
+#     for i in citations:
+#         temp=len(np.where(np.array(citations)>=i))
+#         print(temp)
+#         if temp>=i & len(citations)-temp<=i:
+#             return i
 def solution(citations):
     citations.sort(reverse=True)
-    print(citations)
+    #print(citations)
     for i in range(len(citations)):
         if i+1>citations[i]:
             return i
