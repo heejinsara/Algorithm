2c442d7337c392e4a0cb1377ac44108d1762f03a
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-07 01:44:53 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,16 @@
+def solution(genres, plays):
+    D={}
+    answer=[]
+    for idx, g in enumerate(genres):
+        if g not in D:
+            D[g]=[[idx],plays[idx]]
+        else:
+            D[g][0].append(idx)
+            D[g][1]+=plays[idx]
+    D=dict(sorted(D.items(),key=lambda x:x[1][1], reverse=True))
+    for g in genres:
+        D[g][0].sort(key=lambda x:plays[x], reverse=True)
+    #print(D)
+    for v in D.values():
+        answer.extend(v[0][:2])
+    return answer
