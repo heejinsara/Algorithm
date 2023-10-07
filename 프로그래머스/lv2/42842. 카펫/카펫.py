4156f65d46d1840e6727447464fdf6621c6fa72c
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-07 11:11:26 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,5 +1,5 @@
 def solution(brown, yellow):
     for i in range(1,yellow+1):
         if yellow%i==0:
-            if 2*(i+yellow//i)+4==brown:
+            if 4+2*(i+yellow//i)==brown:
                 return [yellow//i+2,i+2]
