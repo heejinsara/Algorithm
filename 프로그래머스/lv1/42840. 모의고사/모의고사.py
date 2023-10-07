d903718a8779a6c151edee31a242755b150f4883
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-07 10:43:35 +0900

    [level 1] Title: 모의고사, Time: 5.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,19 +1,20 @@
 import math
 def solution(answers):
-    a1=[1, 2, 3, 4, 5]
-    a2=[2, 1, 2, 3, 2, 4, 2, 5]
-    a3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    cnt=[0,0,0]
+    result=[0,0,0]
     answer=[]
+    L1=[1,2,3,4,5]
+    L2=[2,1,2,3,2,4,2,5]
+    L3=[3,3,1,1,2,2,4,4,5,5]
     for i in range(len(answers)):
-        if a1[i%len(a1)]==answers[i]:
-            cnt[0]+=1
-        if a2[i%len(a2)]==answers[i]:
-            cnt[1]+=1
-        if a3[i%len(a3)]==answers[i]:
-            cnt[2]+=1
-    for j in range(len(cnt)):
-        if cnt[j]==max(cnt):
-            answer.append(j+1)
-    answer.sort()
+        if L1[i%len(L1)]==answers[i]:
+            result[0]+=1
+    for i in range(len(answers)):
+        if L2[i%len(L2)]==answers[i]:
+            result[1]+=1
+    for i in range(len(answers)):
+        if L3[i%len(L3)]==answers[i]:
+            result[2]+=1
+    for i in range(len(result)):
+        if result[i]==max(result):
+            answer.append(i+1)
     return answer
