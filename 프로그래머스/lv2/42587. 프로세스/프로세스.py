886c5e5a4bacb57d3632a5a257c30924bfb33c81
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-07 09:54:11 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,11 +1,11 @@
 from collections import deque
 def solution(priorities, location):
-    que=deque((i,j) for i,j in enumerate(priorities))
+    priorities=deque((i,j) for i,j in enumerate(priorities))
     answer=0
-    while que:
-        temp=que.popleft()
-        if any(temp[1]<q[1] for q in que):
-            que.append(temp)
+    while priorities:
+        temp=priorities.popleft()
+        if any(temp[1]<p[1] for p in priorities):
+            priorities.append(temp)
         else:
             answer+=1
             if temp[0]==location:
