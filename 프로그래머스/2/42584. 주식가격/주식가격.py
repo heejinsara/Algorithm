941fcf4da29fe0412e4640b731bd18f363a2916d
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-20 15:49:55 +0900

    [level 2] Title: 주식가격, Time: 37.32 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,13 @@
+from collections import deque
+def solution(prices):
+    prices=deque(prices)
+    answer=[]
+    while prices:
+        cnt=0
+        temp=prices.popleft()
+        for p in prices:
+            cnt+=1
+            if temp>p:
+                break
+        answer.append(cnt)
+    return answer
