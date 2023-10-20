4cc48e976b40c4a0a4045d172c6c83ddc5814d6c
Heejin Park <91619301+heejinsara@users.noreply.github.com>
2023-10-20 16:57:15 +0900

    [level 2] Title: 소수 찾기, Time: 0.30 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+from itertools import permutations
+def isprime(N):
+    if N<=1:
+        return False
+    for i in range(2,int(N**0.5)+1):
+        if N%i==0:
+            return False
+    return True
+
+def solution(numbers):
+    result=[]
+    for i in range(1, len(numbers)+1):
+        for j in permutations(list(numbers),i):
+            temp=int(''.join(j))
+            if isprime(temp):
+                result.append(temp)
+    return len(set(result))
